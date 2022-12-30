b1201bc808c74e88779b47930e8c949d54cd18a2
yxuan0329 <xuan94357@gmail.com>
2022-12-30 16:25:21 +0800

    feat: two hands recognition in Unity and fix the position offset

--- Hand-Tracking/Assets/HandTracking.cs
+++ Hand-Tracking/Assets/HandTracking.cs
@@ -16,49 +16,43 @@ public class HandTracking : MonoBehaviour
     void Update()
     {
         string data = udpReceive.data;
-        // string data2 = udpReceive.data2;
 
         data = data.Remove(0, 1); // remove left bracket
         data = data.Remove(data.Length - 1, 1);// remove right bracket
-        // print(data);
         string[] points = data.Split(',');
-        // print(points[0]);
-
-        /*
-        data2 = data2.Remove(0, 1); // remove left bracket
-        data2 = data2.Remove(data2.Length - 1, 1);// remove right bracket
-        // print(data);
-        string[] points2 = data2.Split(',');
-        // print(points[0]);*/
 
 
         // store (x, y, z) to each point
         //0        1*3      2*3
         //x1,y1,z1,x2,y2,z2,x3,y3,z3
 
-        for (int i = 0; i < 21; i++)
+        if (data.Length > 0)
         {
+            for (int i = 0; i < 21; i++)
+            {
 
-            float x = 7 - float.Parse(points[i * 3]) / 80;
-            float y = float.Parse(points[i * 3 + 1]) / 90;
-            float z = float.Parse(points[i * 3 + 2]) / 100;
+                float x = 9 - float.Parse(points[i * 3]) / 30;
+                float y = float.Parse(points[i * 3 + 1]) / 50;
+                float z = float.Parse(points[i * 3 + 2]) / 50;
 
-            handPoints[i].transform.localPosition = new Vector3(x, y, z);
+                Debug.Log("x=" + x + ", y=" + y + ", z=" + z);
+                handPoints[i].transform.localPosition = new Vector3(x, y, z);
 
+            }
         }
-        /*if (data.Length > 21) // if second hand exists
+        if (data.Length > 63) // if second hand exists
         {
             for (int i = 21; i < 42; i++)
             {
 
-                float x = 7 - float.Parse(points[i * 3]) / 80;
-                float y = float.Parse(points[i * 3 + 1]) / 90;
-                float z = float.Parse(points[i * 3 + 2]) / 100;
+                float x = 9 - float.Parse(points[i * 3]) / 30;
+                float y = float.Parse(points[i * 3 + 1]) / 50;
+                float z = float.Parse(points[i * 3 + 2]) / 50;
 
                 handPoints2[i-21].transform.localPosition = new Vector3(x, y, z);
 
             }
-        }*/
+        }
         
 
 
